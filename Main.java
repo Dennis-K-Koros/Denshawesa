e69d2ad30d5f3e3b71cf900182e98e47acf343ae
ShadrachAroni <126606301+ShadrachAroni@users.noreply.github.com>
2023-06-29 14:13:48 +0300

    trial and error

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("trial");
+        System.out.println("trial and error");
         System.out.println("Let him cook");
     }
 }
